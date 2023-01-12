2da3eac3a7a92d1596c264b05f6639b5cff95300
George Butco <57345333+George-debug@users.noreply.github.com>
2023-01-12 15:19:48 +0200

    sniffer documentation

--- sniffer/Sniffer.py
+++ sniffer/Sniffer.py
@@ -1,40 +1,25 @@
 import socket
 from sniffer.packages.InternetProtocolPacket import InternetProtocolPacket
 from typing import Callable
-import struct
-
-# sniff HTTP traffic
 
 
 class Sniffer:
+    """
+    It captures the all the packets from the eth0 network. At the moment only Linux implementation is available.
+    """
+
     def __init__(self, handle_packet: Callable[[InternetProtocolPacket], None]):
+        """
+        Args:
+            handle_packet (Callable[[InternetProtocolPacket], None]): A function that will be called when a packet is captured.
+        """
         self._handle_packet = handle_packet
         self.__is_running = False
 
-    # =============== windows attempt
-    # def start(self):
-    #     # https://docs.python.org/3/library/socket.html
-    #     # for windows:
-    #     # the public network interface
-    #     host = socket.gethostbyname(socket.gethostname())
-
-    #     # create a raw socket and bind it to the public interface
-    #     conn = socket.socket(
-    #         socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
-    #     # conn.bind((host, 0))
-    #     conn.bind(("", 0))
-
-    #     # Include IP headers
-    #     conn.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
-
-    #     # receive all packets
-    #     conn.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
-
-    #     while True:
-    #         raw_data, addr = conn.recvfrom(65535)
-    #         self.__handle_data(raw_data)
-
     def start(self):
+        """
+        Starts the sniffer, blocking the current thread
+        """
         self.__is_running = True
         # https://docs.python.org/3/library/socket.html
         # for linux:
@@ -49,6 +34,14 @@ class Sniffer:
             self.__handle_data(raw_data)
 
     def __handle_data(self, raw_data: bytes):
+        """
+        This function is called every time a packet is captured. It will ignore the Ethernet header,
+        convert It to InternetProtocolPacket and pass the rest of the packet to the handle_packet function.
+        """
+
+        if not self.__is_running:
+            return
+
         try:
             packet = InternetProtocolPacket(raw_data[14:])
 
@@ -57,4 +50,7 @@ class Sniffer:
             pass
 
     def stop(self):
+        """
+        Stops the sniffer
+        """
         self.__is_running = False
