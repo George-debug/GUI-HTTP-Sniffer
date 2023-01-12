f6c1c591ec8ee7eb4ff3508b4dea40f959b70308
George Butco <57345333+George-debug@users.noreply.github.com>
2023-01-12 12:12:04 +0200

    http filter

--- FilterHTTP.py
+++ FilterHTTP.py
@@ -1,3 +1,5 @@
+import threading
+from datetime import datetime
 from typing import Callable
 from typing import List
 
@@ -24,12 +26,44 @@ class HTTPPRequest:
         self.headers = headers
 
 
+def get_current_time() -> int:
+    curr_dt = datetime.now()
+    return int(round(curr_dt.timestamp()))
+
+
 class FilterHTTP:
     def __init__(self):
         self.__packets: List[HTTPPRequest] = []
+        self.filters: List[Callable[[HTTPPRequest], bool]] = []
+        # self.__lock = threading.Lock()
 
     def add_packet(self, packet: bytes):
         try:
-            self.__packets.append(HTTPPRequest(packet))
+            request = HTTPPRequest(packet)
+            for filter in self.filters:
+                if not filter(request):
+                    return
+            self.__packets.append(request)
         except:
             pass
+
+    def clear(self):
+        self.__packets.clear()
+
+    def next(self) -> HTTPPRequest:
+        if len(self.__packets) == 0:
+            return None
+
+        return self.__packets.pop(0)
+
+    def add_contains_filter(self, key: str, value: str):
+        def filter(request: HTTPPRequest) -> bool:
+            return key in request.headers and value in request.headers[key]
+
+        self.filters.append(filter)
+
+    def add_equals_filter(self, key: str, value: str):
+        def filter(request: HTTPPRequest) -> bool:
+            return key in request.headers and request.headers[key] == value
+
+        self.filters.append(filter)
